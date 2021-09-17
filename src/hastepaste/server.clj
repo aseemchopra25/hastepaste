75dd3a2d734e06a83add5f5ef105cb7004cb430d
Aseem Chopra <aseemchopra@protonmail.com>
2021-09-17 16:33:01 +0530

    ✨ initialised server

---
+++ src/hastepaste/server.clj
@@ -0,0 +1,7 @@
+;; To serve our pasted, we need a server component in chanrge of serving HTTP
+;; -URL to which to paste a new paste to
+;; -URL with which to read pastes by identifier 
+;; -Form to present the form 
+
+;; In its simplest form that component wraps the aleph webserver 
+
