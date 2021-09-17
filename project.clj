ab2418469de57be18ad9ea08b319350cb96b1627
Aseem Chopra <aseemchopra@protonmail.com>
2021-09-17 17:09:04 +0530

    ✨ added hastepaste.main

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 ;; full source code can be found here at https://github.com/aseemchopra25/hastepaste 
 (defproject hastepaste "0.1.0-SNAPSHOT"
   :description "A pastebin clone made using Clojure, Component, Bidi, Aleph and Hiccup"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/aseemchopra25/hastepaste"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://github.com/aseemchopra25/hastepaste"}
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
   ;; Closure is a dynamic general purpose programming language, it's a compiled language but remains dynamic 
   ;; Aleph is for the webserver itself. It has a convenient HTTP client which can leverage the same thread pool
   ;; Bidi is for the routing library, it's birectional 
@@ -14,7 +14,7 @@
   ;; Components needed: 
   ;; webserver to handle aleph startup and bidi routes 
   ;; in memory store for pasted data to live - component instead of cuntion to save in real database
-
+  :main hastepaste.main
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [com.stuartsierra/component "0.3.2"]
                  [aleph "0.4.6"]
